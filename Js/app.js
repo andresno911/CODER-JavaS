9c95ae15f9f3c18224f8b055fc0398c4ebb764c2
Andres Ortega <andresno911@gmail.com>
2023-08-23 21:23:49 -0300

    AGREGO CODIGO PARA CREAR,  VERIFICAR E INGRESO DE  USUARIO

--- Js/app.js
+++ Js/app.js
@@ -1,67 +1,105 @@
+//CREO ARRAY PARA ALMACENAR USUARIOS REGISTRAODS
+const usuarios = [];
 
-// Definición de la función calcularGanancia que calcula la ganancia multiplicando el monto, la tasa y los días
-function calcularGanancia(monto, tasa, dias) {
-    return monto * tasa * dias; 
+function verificoUsuario(nombreUsuario) {
+    // UTILIZO METODO .some() PARA QUE NO SE REPITAN USUARIOS
+    return usuarios.some(usuario => usuario.usuario === nombreUsuario);
 }
-
-// Inicio bucle do-while que permite repetir la simulación
-do {
-    
-    alert("Bienvenido a la Simulación de plazos fijos.");
-
-    // Solicitar eleccion de opción
-    const opcion = parseInt(prompt("Que operacion quiere realizar? \n1. Realizar plazo fijo en pesos \n2. Realizar plazo fijo en dólares \n3. Salir"));
-
-    // Aplico switch para manejar diferentes casos basados en la opción elegida por el usuario
-    switch (opcion) {
-        case 1:
-            // Solicita ingresar monto en pesos y los días del plazo; Defino constante tasaInteresanual
-            let montoPesos = parseInt(prompt("Ingrese el monto en pesos:"));
-            let diasPlazoPesos = parseInt(prompt("Ingrese los días del plazo:"));
-            const tasaInteresanual = 0.97;
-
-            // Aplico If para verificar si el monto y los días cumplen con los requisitos mínimos
-            if (montoPesos >= 20000 && diasPlazoPesos >= 30) {
-                // Calcula la tasa de interés diaria y la ganancia usando la función calcularGanancia
-                const tasaInteres = tasaInteresanual / 365;
-                const ganancia = calcularGanancia(montoPesos, tasaInteres, diasPlazoPesos);
-                // Muestra la ganancia
-                alert(`Plazo fijo en pesos realizado. Ganancia: ${ganancia}`);
-            } else {
-                // Muestra un mensaje de error si los requisitos mínimos no se cumplen
-                alert("Monto mínimo para plazo fijo en pesos es $20000 y los días deben ser mayores a 30.");
-            }
-            break;
-
-        case 2:
-            // Solicita ingresar monto en dolares y los días del plazo; Defino constante tasaInteresanual
-            let montoDolares = parseInt(prompt("Ingrese el monto en dólares:"));
-            let diasPlazoDolares = parseInt(prompt("Ingrese los días del plazo:"));
-            const tasaInteresanualDolar = 0.0050;
-
-            // Aplico If para verificar si el monto y los días cumplen con los requisitos mínimos
-            if (montoDolares >= 100 && diasPlazoDolares >= 30) {
-                // Calcula la tasa de interés diaria y la ganancia usando la función calcularGanancia
-                const tasaInteresDolares = tasaInteresanualDolar / 365;
-                const gananciaDolares = calcularGanancia(montoDolares, tasaInteresDolares, diasPlazoDolares);
-                // Muestra la ganancia
-                alert(`Plazo fijo en dólares realizado. Ganancia: ${gananciaDolares}`);
-            } else {
-                // Muestra un mensaje de error si los requisitos mínimos no se cumplen
-                alert("Monto mínimo para plazo fijo en dólares es $100 y los días deben ser mayores a 30.");
-            }
-            break;
-
-        case 3:
-            // Muestra un mensaje de salida
-            alert("Saliendo...");
-            break;
-
-        default:
-            // Muestra un mensaje de error para opciones inválidas
-            alert("Opción inválida.");
+// FUNCION PARA REGISTRAR USUARIO
+function registrar() {
+    const usuario = prompt("Ingrese un nombre de usuario (mínimo 8 caracteres):");
+    const contraseña = prompt("Ingrese una contraseña (mínimo 8 caracteres):");
+    // USO if PARA COMPROBAR QUE LOS DATOS REGISTRADOS CUMPLEN LOS REQUISITOS
+    if (usuario.length >= 8 && contraseña.length >= 8) {
+        // SI EL USUARIO NO EXISTE SE AGREGA USANDO .push() AL array usuarios
+        if (!verificoUsuario(usuario)) {
+            usuarios.push({ usuario, contraseña });
+            alert("Registro exitoso");
+            console.log("Registro exitoso");
+            console.log("Usuarios actuales:", usuarios);
+        } else {
+            alert("El nombre de usuario ya está registrado");
+            console.log("El nombre de usuario ya está registrado");
+        }
+    } else {
+        alert("Por favor, complete todos los campos usando un mínimo de 8 caracteres");
+        console.log("Por favor, complete todos los campos usando un mínimo de 8 caracteres");
     }
-    
-    // Solicita si se desea realizar otra simulación
-    entramos = parseInt(prompt("Quieres generar otra simulacion? 1 para si"));
-} while (entramos === 1); // El bucle se repetirá mientras la variable 'entramos' sea igual a 1
+}
+// DEFINO FUNCION CALCULAR GANANCIA
+function calcularGanancia(monto, tasa, dias) {
+    return monto * tasa * dias;
+}
+//USO while PARA DESARROLLAR LAS OPCIONES DE REGISTRO E INGRESO, EN case 2 TRAS INGRESAR DESARROLLO EL SIMULADOR DE PLAZO FIJO
+while (true) {
+    const opcion = prompt("Elija una opción:\n1. Registrar\n2. Ingresar\n3. Salir");
+
+    switch (opcion) {
+        case "1":
+            registrar();
+            break;
+        case "2":
+            const usuario = prompt("Ingrese su nombre de usuario:");
+            const contraseña = prompt("Ingrese su contraseña:");
+
+            //UTILIZO METODO .find() PARA VER SI EL USUARIO ESTA REGISTRADO
+            const usuarioEncontrado = usuarios.find(u => u.usuario === usuario);
+
+            if (usuarioEncontrado && usuarioEncontrado.contraseña === contraseña) {
+                alert("Inicio de sesión exitoso");
+                console.log("Inicio de sesión exitoso");
+
+                do {
+                    alert("Bienvenido a la Simulación de plazos fijos.");
+                    const opcion = parseInt(prompt("Que operacion quiere realizar? \n1. Realizar plazo fijo en pesos \n2. Realizar plazo fijo en dólares \n3. Salir"));
+                    // APLICO SWITCH PARA DESAROLLAR LAS OPCIONES QUE PUEDE ELEGIR EL USUARIO
+                    switch (opcion) {
+                        case 1:
+                            let montoPesos = parseInt(prompt("Ingrese el monto en pesos:"));
+                            let diasPlazoPesos = parseInt(prompt("Ingrese los días del plazo:"));
+                            const tasaInteresanual = 1.18;
+
+                            // APLICO if PARA VER SI LOS MONTOS CUMPLEN REQUISITOS
+                            if (montoPesos >= 20000 && diasPlazoPesos >= 30) {
+                                const tasaInteres = tasaInteresanual / 365;
+                                const ganancia = calcularGanancia(montoPesos, tasaInteres, diasPlazoPesos);
+                                alert(`Plazo fijo en pesos realizado. Ganancia: ${ganancia}`);
+                            } else {
+                                alert("Monto mínimo para plazo fijo en pesos es $20000 y los días deben ser mayores a 30.");
+                            }
+                            break;
+                        case 2:
+                            let montoDolares = parseInt(prompt("Ingrese el monto en dólares:"));
+                            let diasPlazoDolares = parseInt(prompt("Ingrese los días del plazo:"));
+                            const tasaInteresanualDolar = 0.0050;
+                            // APLICO if PARA VER SI LOS MONTOS CUMPLEN REQUISITOS
+                            if (montoDolares >= 100 && diasPlazoDolares >= 30) {
+                                const tasaInteresDolares = tasaInteresanualDolar / 365;
+                                const gananciaDolares = calcularGanancia(montoDolares, tasaInteresDolares, diasPlazoDolares);
+                                alert(`Plazo fijo en dólares realizado. Ganancia: ${gananciaDolares}`);
+                            } else {
+                                alert("Monto mínimo para plazo fijo en dólares es $100 y los días deben ser mayores a 30.");
+                            }
+                            break;
+                        case 3:
+                            alert("Saliendo...");
+                            break;
+                        default:
+                            alert("Opción inválida.");
+                    }
+                    entramos = parseInt(prompt("¿Quieres generar otra simulación? 1 para sí"));
+                } while (entramos === 1);
+
+            } else {
+                alert("Usuario o contraseña incorrectos");
+                console.log("Usuario o contraseña incorrectos");
+            }
+            break;
+        case "3":
+            alert("¡Hasta luego!");
+            break;
+        default:
+            alert("Opción inválida");
+            break;
+    }
+}
